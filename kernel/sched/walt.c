8603a8abfd393a68adf4271362221591cf94d047
albert.zl_huang <albert.zl_huang@htc.com>
2016-07-12 19:03:46 +0800

    arch_timer: add error handling when the MPM global timer is cleared
    Bug: 29000863
    Signed-off-by: albert.zl_huang <albert.zl_huang@htc.com>
    Change-Id: I2b5a28b0a9edb31bdaa1ca2310397dd2f36f6c23

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -186,6 +186,7 @@ static int __init set_walt_ravg_window(char *str)
 
 early_param("walt_ravg_window", set_walt_ravg_window);
 
+extern u64 arch_counter_get_cntpct(void);
 static void
 update_window_start(struct rq *rq, u64 wallclock)
 {
@@ -200,6 +201,12 @@ update_window_start(struct rq *rq, u64 wallclock)
 	}
 
 	BUG_ON(delta < 0);
+		if (arch_counter_get_cntpct() == 0)
+			delta = 0;
+		else
+			BUG_ON(1);
+	}
+
 	if (delta < walt_ravg_window)
 		return;
 
